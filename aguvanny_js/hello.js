db68b44ddb63253b6971bbb413138889798890f9
ygbadamosi662 <gbadamosiy662@gmail.com>
2025-02-20 11:58:33 +0100

    html js dom

---
+++ aguvanny_js/hello.js
@@ -0,0 +1,12 @@
+let fname = "vannesa";
+fname = "Vee";
+let lname;
+const gender = "female";
+
+function minus(num1, num2) {
+    const result = num1 - num2;
+    console.log(result);
+    // return result;
+}
+
+minus(10, 50);
